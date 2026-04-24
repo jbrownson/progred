4b0fa87afdbe197bae4660c852435b2792496afe
Jake Brownson <jbrownson@gmail.com>
2026-04-23 21:00:49 -0700

    Move Vite configs into subdirectory

--- prototype-ts/vite/renderer.config.ts
+++ prototype-ts/vite/renderer.config.ts
@@ -1,6 +1,6 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { progredDataPlugin } from "./vite.progred"
+import { progredDataPlugin } from "./progred"
 
 export default defineConfig({
   base: "./",
